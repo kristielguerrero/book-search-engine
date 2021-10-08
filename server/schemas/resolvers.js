e8ada6dc1d8288ae86fb852a385d8899833e42ba
Kris Tina <kristyguerrero20@gmail.com>
2021-10-08 15:23:02 -0500

    finished the resolver query in resovers file

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,17 @@
 //  Define the query and mutation functionality to work with the Mongoose models.
 
 const { AuthenticationError } = require('apollo-server-express');
-const { User, Book } = require('../models');
+const { Book, User } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
-  Query: {},
+  Query: {
+    me: async (parent, args, context) => {
+      const userData = await User.findOne({ _id: context.user._id });
+      if (!userData) {
+        return res.status(400).json({ message: 'User with this ID not found' });
+      }
+      return userData;
+    },
+  },
 };
