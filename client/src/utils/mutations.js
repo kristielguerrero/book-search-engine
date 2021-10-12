69856bc2068e6ab55fe741cbe4244b4c0f045844
Kris Tina <kristyguerrero20@gmail.com>
2021-10-12 13:08:47 -0500

    finished the queries in mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,6 +1,6 @@
 import { gql } from '@apollo/client';
 
-export const LOGIN_USER = global`
+export const LOGIN_USER = gql`
 mutation login($emal: String, password: String){
     login(email: $email, password: $password) {
         token
@@ -11,3 +11,55 @@ mutation login($emal: String, password: String){
     }
 }
 `;
+
+export const ADD_USER = gql`
+mutation addUser($username: String, $email: String, $password: String){
+    addUser(username: $username, email: $email, password: $password) {
+        token 
+        user {
+            _id
+            username
+            email
+            bookCount
+            savedBooks{
+                savedBooks: [book]
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+}
+`;
+
+export cont REMOVE_BOOK = gql`
+mutation saveBook($newBook: bookInfo){
+    saveBook (newBook: $newBook){
+        bookIdauthors
+        description
+        title
+        image
+        link
+    }
+}
+`; 
+
+export const REMOVE_BOOK = gql`
+mutation removeBook($bookId: String){
+    removeBook(bookId: $bookId){
+        _id
+        username
+        email
+        savedBooks{
+            savedBooks: [book]
+            bookId 
+            authorsdescription
+            titleimage
+            link
+        }
+    }
+}
+`; 
